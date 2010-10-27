5bcee9cb5e1e9f3e52f7d520158bd37993dcac68
Robbie <rp9372@bristol.ac.uk>
2010-10-27 16:08:42 +0100

    Cleaned up git testing stuff.

--- src/Streamberry.java
+++ src/Streamberry.java
@@ -3,11 +3,8 @@ public class Streamberry {
 	/**
 	 * @param args
 	 */
-	// This is a comment added by Jake to test git
 	public static void main(String[] args) {
 		System.out.println("Hello, Streamberry user!");
-		
-		System.out.println("Robbie added this");
 	}
 
 }
