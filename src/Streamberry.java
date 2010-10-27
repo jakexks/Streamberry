7bc0ef6a0200c22d057ffc2e8a4c80575f4a4edf
Robbie <rp9372@bristol.ac.uk>
2010-10-27 15:54:41 +0100

    Testing adding streamberry

--- src/Streamberry.java
+++ src/Streamberry.java
@@ -4,7 +4,7 @@ public class Streamberry {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, Streamberry user!");
 
 	}
 
