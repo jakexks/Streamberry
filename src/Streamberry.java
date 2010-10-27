9ef30f674329af9903813bff6ec4511400dceb23
Jake <jake@familysanders.net>
2010-10-27 15:56:48 +0100

    Added a comment to Streamberry.java

--- src/Streamberry.java
+++ src/Streamberry.java
@@ -3,6 +3,7 @@ public class Streamberry {
 	/**
 	 * @param args
 	 */
+	// This is a comment added by Jake to test git
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 
