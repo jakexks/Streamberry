b35491a7815657147fe64c1e5509ed7fc84d094d
Jake <jake@familysanders.net>
2010-10-28 15:43:02 +0100

    Experimented with threading

--- src/com/streamberry/PoC_Discovery.java
+++ src/com/streamberry/PoC_Discovery.java
@@ -1,9 +1,27 @@
 package com.streamberry;
 
-public class PoC_Discovery {
-	public static void main(String[] args) {
-		System.out.println("Hello, networking world!");
-		
+public class PoC_Discovery implements Runnable {
+	// public static void main(String[] args) {
+	// new Thread(new PoC_Discovery()).start();
+	//		
+	// }
 
+	@Override
+	public void run() {
+		try {
+			printwithdelay("Hello", 1);
+			printwithdelay("World", 2);
+			printwithdelay("Stuff", 3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
+	private static void printwithdelay(String thing, int id) throws InterruptedException {
+		Thread.sleep(1000);
+		System.out.println(thing + "\nI am thread" + id +"\n-------------");
+		
+	}
+
 }
