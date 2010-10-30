79557d5f92027784f514da57ebcfea73973a56a2
Jake <jake@familysanders.net>
2010-10-30 04:33:50 +0100

    Fleshed out the Proof of Concept reciever class, it should now actually do things.

--- src/com/streamberry/PoC_Receiver.java
+++ src/com/streamberry/PoC_Receiver.java
@@ -1,11 +1,41 @@
 package com.streamberry;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class PoC_Receiver implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		int port = 35489;
+
+		System.out.println("Receiver thread started");
+		listener(port);
+	}
+
+	private void listener(int port) {
+		byte recvbuf[] = new byte[1024];
+		// Arbitrarily chosen from unassigned multicast block
+		// See:
+		// http://www.iana.org/assignments/multicast-addresses/multicast-addresses.xml
+		String multicastgroup = "224.0.0.133";
+
+		try {
+			MulticastSocket s = new MulticastSocket(port);
+
+			s.joinGroup(InetAddress.getByName(multicastgroup));
+			DatagramPacket p = new DatagramPacket(recvbuf, recvbuf.length);
+			for (;;) {
+				s.receive(p);
+				System.out.println("Recieved data from " + p.getAddress().getCanonicalHostName());
+			}
+		} catch (IOException e) {
+			System.err.println("Failed to make multicast socket (port " + port);
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
