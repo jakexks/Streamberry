2060b84a1fffc4b4d3c4f6dc591b4cd4855934f8
Jake <jake@familysanders.net>
2010-11-01 11:54:15 +0000

    More windows related MAC debugging

--- src/com/streamberry/PoC_Sender.java
+++ src/com/streamberry/PoC_Sender.java
@@ -32,12 +32,12 @@ public class PoC_Sender implements Runnable {
 				NetworkInterface ni = nics.nextElement();
 				System.out.print("Found network adaptor " + ni.getName() + " ");
 				byte mac[] = ni.getHardwareAddress();
+				for (int i = 0; i < mac.length; i++)
+					System.out.format("%02X%s", mac[i],
+							(i < mac.length - 1) ? "-" : "\n");
 				if (mac == null) {
 					// No MAC address, probably loopback
 					System.err.println("No MAC ???");
-					for (int i = 0; i < mac.length; i++)
-						System.out.format("%02X%s", mac[i],
-								(i < mac.length - 1) ? "-" : "\n");
 					System.exit(1);
 				} else {
 					uniqid = mac;
