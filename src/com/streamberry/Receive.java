ea3549c3f1f4f0bffedf72f0e1bfdc29bf5bc0fc
Jake <jake@familysanders.net>
2010-11-03 13:55:46 +0000

    Started work on maintaining a list of clients

--- src/com/streamberry/Receive.java
+++ src/com/streamberry/Receive.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Hashtable;
+
+import javax.sql.rowset.serial.SerialArray;
 
 public class Receive implements Runnable {
 
@@ -12,6 +17,8 @@ public class Receive implements Runnable {
 	// See:
 	// http://www.iana.org/assignments/multicast-addresses/multicast-addresses.xml
 	String multicastgroup = "224.0.0.133";
+
+	Hashtable<byte[], Date> clienttable = new Hashtable<byte[], Date>();
 
 	@Override
 	public void run() {
@@ -43,15 +50,27 @@ public class Receive implements Runnable {
 				+ p.getAddress().getCanonicalHostName());
 		byte data[] = p.getData();
 		if (data[0] == (byte) 0xFF && data[1] == (byte) 0xFF) {
-			new Thread(new Beacon(data)).start();
+			new Thread(new Beacon(data, clienttable)).start();
 		}
 	}
 
 }
 
 class Beacon extends Thread {
-	public Beacon(byte[] data) {
+	public Beacon(byte[] data, Hashtable<byte[], Date> ht) {
 		byte id[] = { data[2], data[3], data[4], data[5], data[6], data[7] };
+
+		if (ht.containsKey(id)) {
+			ht.remove(id);
+			Calendar c = Calendar.getInstance();
+			ht.put(id, c.getTime());
+			System.out.println("updated ht");
+		} else {
+			Calendar c = Calendar.getInstance();
+			ht.put(id, c.getTime());
+			System.out.println("added to ht");
+		}
+
 		System.out.print("Hello (Keep-alive) packet from ");
 		for (int i = 0; i < id.length; i++) {
 			System.out
