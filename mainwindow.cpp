411e58c3397327b8763ccf70e6677c715293210f
Robbie <rp9372@bristol.ac.uk>
2010-11-06 02:25:31 +0000

    Played with GUI.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QGraphicsView>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    canvas = new QGraphicsView;
+    setCentralWidget(canvas);
+    this->setStatusBar(0);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete canvas;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
 
 namespace Ui {
     class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsView *canvas;
 };
 
 #endif // MAINWINDOW_H
