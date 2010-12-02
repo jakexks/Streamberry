9694193652377b850212cd0efc54080fb0128ed1
Vitaliy <vk8890@bris.ac.uk>
2010-12-02 13:13:30 +0000

    Added main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* libVLC and Qt sample code
+ * Copyright © 2009 Alexander Maringer <maringer@maringer-it.de>
+ */
+
+#include "vlc_on_qt.h"
+#include "file_meta.h"
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[])
+{
+
+    //File_Meta a;
+    //a.print_meta("./01-Metric-Help I'm Alive.mp3"); //Replace with file name to print Title
+    QApplication a(argc, argv);
+    Player p;
+    p.resize(640,480);
+    p.playFile("./01-Metric-Help I'm Alive.mp3"); // Replace with what you want to play
+    p.show();
+    return a.exec();
+}
