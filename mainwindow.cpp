aa9b79cbf5a4c9db9eb5c269949d60c75ad1eccd
Robbie <rp9372@bristol.ac.uk>
2011-01-29 22:42:53 +0000

    Bottom bar sort of done. Enough for today anyway. Just needs aligning.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -155,13 +155,99 @@ QWidget* MainWindow::makeBottomBar() {
 
     QGridLayout *temp = new QGridLayout(tempw);
     temp->setSpacing(0);
-    temp->setMargin(5);
+    temp->setMargin(0);
+
+    //playback slider
     QHBoxLayout *playbackbox = new QHBoxLayout();
     playbackbox->setContentsMargins(15, 0, 15, 0);
     QSlider *playback = new QSlider(Qt::Horizontal);
     playbackbox->addWidget(playback);
-    temp->addLayout(playbackbox, 0, 0, Qt::AlignHCenter);
 
+    //playback controls
+    QGridLayout* controls = new QGridLayout();
+    QHBoxLayout* left = new QHBoxLayout();
+    QHBoxLayout* middle = new QHBoxLayout();
+    QHBoxLayout* right = new QHBoxLayout();
+    left->setSpacing(35);
+    left->setMargin(0);
+    left->setContentsMargins(0,0,35,0);
+    middle->setSpacing(0);
+    middle->setContentsMargins(0,0,0,0);
+    middle->setMargin(0);
+    right->setSpacing(10);
+    right->setContentsMargins(200,0,0,0);
+    right->setMargin(0);
+    controls->addLayout(left, 0, 1, Qt::AlignRight | Qt::AlignVCenter);
+    controls->addLayout(middle, 0, 2, Qt::AlignCenter | Qt::AlignVCenter);
+    controls->addLayout(right, 0, 3, Qt::AlignLeft | Qt::AlignVCenter);
+    controls->setSpacing(0);
+    controls->setMargin(0);
+    controls->setColumnStretch(0, 1);
+    controls->setColumnStretch(1, 0);
+    controls->setColumnStretch(2, 0);
+    controls->setColumnStretch(3, 0);
+    controls->setColumnStretch(4, 1);
+
+    QPushButton *repeat = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnRepeatUp.png); background-repeat: no-repeat; border:none;";
+    repeat->setStyleSheet(style);
+    repeat->setMaximumSize(57, 36);
+    repeat->setMinimumSize(57, 36);
+    repeat->setFlat(true);
+    QPushButton *shuffle = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnShuffleUp.png); background-repeat: no-repeat; border:none;";
+    shuffle->setStyleSheet(style);
+    shuffle->setMaximumSize(57, 36);
+    shuffle->setMinimumSize(57, 36);
+    shuffle->setFlat(true);
+    QPushButton *previous = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnPreviousUp.png); background-repeat: no-repeat; border:none;";
+    previous->setStyleSheet(style);
+    previous->setMaximumSize(42, 39);
+    previous->setMinimumSize(42, 39);
+    previous->setFlat(true);
+    QPushButton *playpause = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnPlayUp.png); background-repeat: no-repeat; border:none;";
+    playpause->setStyleSheet(style);
+    playpause->setMaximumSize(48, 59);
+    playpause->setMinimumSize(48, 59);
+    playpause->setFlat(true);
+    QPushButton *next = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnNextUp.png); background-repeat: no-repeat; border:none;";
+    next->setStyleSheet(style);
+    next->setMaximumSize(42, 39);
+    next->setMinimumSize(42, 39);
+    next->setFlat(true);
+    QPushButton *mute = new QPushButton();
+    style = "background-image: url(";
+    style += expath;
+    style += "images/btnMuteUp.png); background-repeat: no-repeat; border:none;";
+    mute->setStyleSheet(style);
+    mute->setMaximumSize(57, 36);
+    mute->setMinimumSize(57, 36);
+    mute->setFlat(true);
+    QSlider *volumeslider = new QSlider(Qt::Horizontal);
+    volumeslider->setFixedWidth(125);
+    left->addWidget(repeat);
+    left->addWidget(shuffle);
+    middle->addWidget(previous);
+    middle->addWidget(playpause);
+    middle->addWidget(next);
+    right->addWidget(mute);
+    right->addWidget(volumeslider);
+
+    temp->addLayout(controls, 1, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+    temp->addLayout(playbackbox, 0, 0, Qt::AlignHCenter | Qt::AlignTop);
     return tempw;
 }
 
