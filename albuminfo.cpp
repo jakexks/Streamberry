d7a186cf44b15a68424f084c7f21c50c8a2dd76e
Jake <jake@familysanders.net>
2011-01-31 16:42:44 +0000

    Some sort of album table view. It's not as good as I would like

--- albuminfo.cpp
+++ albuminfo.cpp
@@ -3,14 +3,14 @@
 #include <QList>
 
 QString albumTitle;
-QString artist;
+QString albumArtist;
 QList<QString> tracks;
 
 albuminfo::albuminfo()
 {
     // Sample data...
     albumTitle = "Shotter's Nation";
-    artist = "Babyshambles";
+    albumArtist = "Babyshambles";
     tracks += "Delivery";
     tracks += "You Talk";
     tracks += "UnBiloTitled";
@@ -22,4 +22,18 @@ albuminfo::albuminfo()
     tracks += "Baddies Boogiex";
     tracks += "Deft Left Hand";
     tracks += "Lost Art of Murder";
+}
+QString albuminfo::getArtist()
+{
+    return albumArtist;
+}
+
+QString albuminfo::getTitle()
+{
+    return albumTitle;
+}
+
+QList<QString> albuminfo::getTracks()
+{
+    return tracks;
 }
--- albuminfo.h
+++ albuminfo.h
@@ -1,10 +1,14 @@
 #ifndef ALBUMINFO_H
 #define ALBUMINFO_H
-
+#include <QList>
+#include <QString>
 class albuminfo
 {
 public:
     albuminfo();
+    QList<QString> getTracks();
+    QString getArtist();
+    QString getTitle();
 };
 
 #endif // ALBUMINFO_H
