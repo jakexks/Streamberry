45a498c3bf4c15cc083514eac3cc1fb2962bf25c
Robbie <rp9372@bristol.ac.uk>
2011-02-24 16:17:33 +0000

    Fixed local library.

--- database.cpp
+++ database.cpp
@@ -18,7 +18,7 @@ Database::Database()
 {
     db = QSqlDatabase::addDatabase( "QSQLITE" );
     connected = false;
-    localUniqueId = "-1"; //////REQUIRES CHANGE this should draw the actually unique ID from networking, but not yet!
+    localUniqueId = "Local"; //////REQUIRES CHANGE this should draw the actually unique ID from networking, but not yet!
     dbfilename = "database.sqlite";
     initialise();
 }
@@ -60,15 +60,15 @@ void Database::createDatabase(QString &path)
     filepath += dbfilename;
     //sql statements which creates structure. must be split as it doesn't seem to work with just one
     QString sql[sqlstatements];
-    sql[0] = "DROP TABLE IF EXISTS \"HomeTable\";";
+    sql[0] = "DROP TABLE IF EXISTS \"LibLocal\";";
     sql[1] = "CREATE TABLE \"Lib";
     sql[1] += localUniqueId;
-    sql[1] += "\" (\"UniqueID\" VARCHAR NOT NULL, \"ID\" INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL UNIQUE, \"Filepath\" VARCHAR NOT NULL  UNIQUE , \"Artist\" VARCHAR, \"Album\" VARCHAR, \"Title\" VARCHAR, \"Track\" INTEGER, \"Genre\" VARCHAR, \"Rating\" INTEGER, \"Filename\" VARCHAR NOT NULL , \"Year\" INTEGER, \"Length\" INTEGER NOT NULL, \"Bitrate\" INTEGER, \"Filesize\" INTEGER, \"Timestamp\" INTEGER NOT NULL , \"Filetype\" VARCHAR, \"Deleted\" BOOL NOT NULL DEFAULT 0);";
+    sql[1] += "\" (\"ID\" INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL UNIQUE, \"Filepath\" VARCHAR NOT NULL  UNIQUE , \"Artist\" VARCHAR, \"Album\" VARCHAR, \"Title\" VARCHAR, \"Track\" INTEGER, \"Genre\" VARCHAR, \"Rating\" INTEGER, \"Filename\" VARCHAR NOT NULL , \"Year\" INTEGER, \"Length\" INTEGER NOT NULL, \"Bitrate\" INTEGER, \"Filesize\" INTEGER, \"Timestamp\" INTEGER NOT NULL , \"Filetype\" VARCHAR, \"Deleted\" BOOL NOT NULL DEFAULT 0);";
     sql[2] = "DROP TABLE IF EXISTS \"LibIndex\";";
     sql[3] = "CREATE TABLE \"LibIndex\" (\"ID\" INTEGER PRIMARY KEY  NOT NULL  UNIQUE  check(typeof(\"ID\") = 'integer') , \"Local\" BOOL NOT NULL  DEFAULT 0, \"TimeLastUpdated\" INTEGER NOT NULL , \"TimeLastOnline\" INTEGER NOT NULL , \"UniqueID\" VARCHAR UNIQUE NOT NULL, \"Name\" VARCHAR NOT NULL , \"Online\" BOOL NOT NULL );";
     sql[4] = "INSERT INTO LibIndex (ID, Local, TimeLastUpdated, TimeLastOnline, UniqueID, Name, Online) VALUES (\"1\", 1, \"";
     sql[4] += QString::number(Utilities::getCurrentTimestamp());
-    sql[4] += "\", \"0\", \"-1\", \"Local\", 1);";
+    sql[4] += "\", \"0\", \"Local\", \"Local\", 1);";
     sql[5] = "DROP TABLE IF EXISTS \"Settings\";";
     sql[6] = "CREATE TABLE \"Settings\" (\"Name\" VARCHAR(10) UNIQUE,\"Value\" INTEGER);";
     sql[7] = "DROP TABLE IF EXISTS \"TrackedFolders\";";
@@ -145,7 +145,6 @@ QSqlQuery Database::query(QString sql)
       if(!query.exec())
       {
           qDebug() << queries.at(i);
-          qDebug() << "here in Database point 5";
           QString s = "SQL failed: ";
           s += query.lastError().text();
           throw SBException(DB, s);
@@ -484,12 +483,12 @@ QList<QSqlRecord>* Database::searchDb(int type, QString searchtxt, QList<QString
         }
     }
 
-    condition += ", TRACK ASC";
+    condition += ", Track ASC";
 
     try
     {
         //select all unique ids from libindex
-        sql = "SELECT UniqueID FROM LibIndex WHERE (UniqueID!=-1) AND (Online=1)";
+        sql = "SELECT UniqueID FROM LibIndex WHERE (Online=1)";
         result = query(sql);
         result.first();
 
