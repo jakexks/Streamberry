8ae57672039bd65760ca8afef817ef26408c24c0
Robbie <rp9372@bristol.ac.uk>
2011-02-24 16:26:12 +0000

    Commented out broken code.

--- main.cpp
+++ main.cpp
@@ -28,11 +28,11 @@ int main(int argc, char *argv[])
   try
   {
 
-    db.setFolders("C:/Users/Jim/Music");
-    Filescan fs(db);
-    QThread fsthread;
-    fs.moveToThread(&fsthread);
-    fs.build_new_clean();
+//    db.setFolders("/Users/Robbie/Music/Albums");
+//    Filescan fs(db);
+//    QThread fsthread;
+//    fs.moveToThread(&fsthread);
+//    fs.build_new_clean();
 
     //importLib itunes("");//file name here.
     //QList<QString> allFiles = itunes.libFileList();
