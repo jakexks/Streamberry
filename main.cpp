3dc6da37bdd81ef257ff6a14ce1067082476f3bd
Jake <jake@familysanders.net>
2011-03-08 16:27:55 +0000

    More hardcoded filescanning :D

--- main.cpp
+++ main.cpp
@@ -61,7 +61,8 @@ int main(int argc, char *argv[])
     //db.setFolders("C:\\Users\\Jim\\Music");
     //db.setFolders("/host/new/");
     //db.setFolders("C:\\Users\\2kah\\Music\\Led Zeppelin");
-    db.setFolders("/streamberrytest/");
+    //db.setFolders("/streamberrytest/");
+    db.setFolders("/home/jakexks/Dropbox/Music/Basshunter/Basshunter - Bass Generation");
 
     MainWindow w(util, db, player, fs);
 
