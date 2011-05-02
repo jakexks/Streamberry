e23fac2f4f9caf8a0bbc79d9552e8a4637057872
Jim <jim.lumsden@gmail.com>
2011-05-02 20:56:46 +0100

    A small change, hope it fixes things

--- playbackcontroller.cpp
+++ playbackcontroller.cpp
@@ -33,6 +33,7 @@ void PlaybackController::setvolumepos(int j)
 QWidget* PlaybackController::makeWidget()
 {
     QWidget *tempw = new QWidget();
+    shufflePressed = false;
     tempw->setObjectName("bottomBarWidget");
     tempw->setMinimumHeight(96);
     tempw->setMaximumHeight(96);
@@ -159,20 +160,15 @@ void PlaybackController::shufflePress()
 {
     if(shufflePressed==true)
     {
-        shufflePressed=false;
         shuffle->setChecked(false);
-        shuffle->setStyleSheet(util.getStylesheet());
-        emit sPress();
-
+        shufflePressed=false;
     }
     else
     {
-        shufflePressed=true;
         shuffle->setChecked(true);
-        shuffle->setStyleSheet(util.getStylesheet());
-        emit sPress();
+        shufflePressed=true;
     }
-
+    emit sPress();
 }
 
 void PlaybackController::repeatPress()
