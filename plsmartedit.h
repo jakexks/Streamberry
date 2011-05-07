1d8ecb997af47a54949e3d4d9e4a895b60372c70
Jim <jim.lumsden@gmail.com>
2011-05-08 00:36:09 +0100

    more comments for my classes

--- plsmartedit.h
+++ plsmartedit.h
@@ -8,10 +8,12 @@ class Playlist;
 class QLineEdit;
 
 
+//This class handles a dialog box used to create edit filter playlists.
 class PLSmartEdit : public QDialog
 {
    Q_OBJECT
 public:
+    //Instantiates the dialog box, passes it a database pointer and utilities so it can be styled
     PLSmartEdit(Playlist* playlist, Utilities* util);
 private slots:
     void btnClicked();
--- plsmartnew.h
+++ plsmartnew.h
@@ -8,18 +8,25 @@ class Playlist;
 class QLineEdit;
 class Database;
 
-
+//This class handles a dialog box used to create new filter playlists.
 class PLSmartNew : public QDialog
 {
    Q_OBJECT
 public:
+  //Instantiates the dialog box, passes it a database pointer and utilities so it can be styled
     PLSmartNew(Utilities* util, Database& datab);
 private slots:
+    //When the button is clicked a new playlist is created using the name
+    //from the first textbox and the filter terms from the second textbox.
     void btnClicked();
 private:
+    //This holds a pointer to the first textbox in the dialog box
     QLineEdit* textbox1;
+    //This holds a pointer to the second textbox in the dialog box
     QLineEdit* textbox2;
+    //pointer to the dialog widget
     QDialog* dialog;
+    //Holds a pointer to the database
     Database& db;
 
 };
